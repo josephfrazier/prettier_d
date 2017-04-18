3dd44ec7dfc6231ad7add2411e51d07def96c7d1
Joseph Frazier <1212jtraceur@gmail.com>
2017-04-18 19:16:26 -0400

    Add basic test

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail # bash "strict mode"
+
+prettier=./bin/prettier.js
+
+$prettier stop
+$prettier start
+
+PORT=`cat ~/.prettier_d | cut -d" " -f1`
+TOKEN=`cat ~/.prettier_d | cut -d" " -f2`
+
+md5sum $prettier | grep f75b2b44fd861a20b69f9a3e1960e419 >/dev/null
+echo "$TOKEN $PWD $prettier" | nc localhost $PORT | md5sum | grep 750573a1ced7ec47055a51584e1fcd6e >/dev/null
