610231c1e3d8b4337ea450f3efc48654a08afdaa
Joseph Frazier <1212jtraceur@gmail.com>
2017-07-08 16:03:52 -0400

    Test/Fix OSX on Travis
    It looks like the OSX version of `nc` doesn't support the `-q` flag,
    so prettier_dnc needs to check nc, and the tests shouldn't use nc directly.

--- bin/prettier_dnc.sh
+++ bin/prettier_dnc.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 PORT=`cat ~/.prettier_d | cut -d" " -f1`
 TOKEN=`cat ~/.prettier_d | cut -d" " -f2`
-cat <(echo "$TOKEN $PWD --stdin $@") - | nc -q 0 localhost $PORT
+if nc 2>&1 | grep -- -q >/dev/null; then
+  cat <(echo "$TOKEN $PWD --stdin $@") - | nc -q 0 localhost $PORT
+else
+  cat <(echo "$TOKEN $PWD --stdin $@") - | nc      localhost $PORT
+fi
