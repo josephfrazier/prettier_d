98144c02511c83d9b83c38323ca2db15f37f7e2b
Joseph Frazier <1212jtraceur@gmail.com>
2018-12-27 12:25:43 -0500

    Remove prettier_d test for `--fallback` option

--- test.sh
+++ test.sh
@@ -28,12 +28,6 @@ cat $file | $prettier_dnc --parser babylon | md5sum | grep 750573a1ced7ec47055a5
 tmp=.write.test.js
 cp $file $tmp && $prettier --write $tmp && $prettier --list-different $tmp | wc -c | grep '\<0$' >/dev/null && rm $tmp
 
-# Ensure --fallback is in the help message
-$prettier --help | grep -- "--fallback" >/dev/null
-# Ensure --fallback with invalid syntax prints the original input
-echo "a.1" | $prettier_dnc --fallback | grep '^a\.1$' >/dev/null
-echo "a.1" | $prettier_dnc --fallback | wc -c | grep '\<4$' >/dev/null
-
 # Verify that multiple files are currently not supported
 $prettier $file $file | md5sum | grep 750573a1ced7ec47055a51584e1fcd6e >/dev/null
 
