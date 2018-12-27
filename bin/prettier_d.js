61c2b98e08bcc99da1fc1bafebb613eb3529e864
Joseph Frazier <1212jtraceur@gmail.com>
2018-12-27 15:36:36 -0500

    Use top-level `return` instead of `else`

--- bin/prettier_d.js
+++ bin/prettier_d.js
@@ -8,38 +8,40 @@ function start() {
 const cmd = process.argv[2];
 if (cmd === "start") {
   start();
-} else {
-  const client = require("../lib/client");
-  if (cmd === "restart") {
-    client.stop(() => {
-      process.nextTick(start);
+  return;
+}
+
+const client = require("../lib/client");
+if (cmd === "restart") {
+  client.stop(() => {
+    process.nextTick(start);
+  });
+  return;
+}
+
+const commands = ["stop", "status", "restart"];
+if (commands.indexOf(cmd) === -1) {
+  const useStdIn = process.argv.indexOf("--stdin") > -1;
+  const args = process.argv.slice(2);
+
+  if (!require("supports-color")) {
+    args.unshift("--no-color");
+  }
+
+  if (useStdIn) {
+    let text = "";
+    process.stdin.setEncoding("utf8");
+
+    process.stdin.on("data", chunk => {
+      text += chunk;
+    });
+
+    process.stdin.on("end", () => {
+      client.lint(args, text);
     });
   } else {
-    const commands = ["stop", "status", "restart"];
-    if (commands.indexOf(cmd) === -1) {
-      const useStdIn = process.argv.indexOf("--stdin") > -1;
-      const args = process.argv.slice(2);
-
-      if (!require("supports-color")) {
-        args.unshift("--no-color");
-      }
-
-      if (useStdIn) {
-        let text = "";
-        process.stdin.setEncoding("utf8");
-
-        process.stdin.on("data", chunk => {
-          text += chunk;
-        });
-
-        process.stdin.on("end", () => {
-          client.lint(args, text);
-        });
-      } else {
-        client.lint(args);
-      }
-    } else {
-      client[cmd](process.argv.slice(3));
-    }
+    client.lint(args);
   }
+} else {
+  client[cmd](process.argv.slice(3));
 }
